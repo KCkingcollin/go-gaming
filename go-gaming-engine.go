c94947254126158851825261b714fa40ddea2ce4
KCkingcollin <kckingcollin99@gmail.com>
2024-12-15 20:18:49 +0100

    paranoid pushing

--- go-gaming-engine.go
+++ go-gaming-engine.go
@@ -8,8 +8,44 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-const winWidth = 1280
-const winHight = 720
+const winWidth = 480
+const winHight = 480
+const fragPath = "shaders/frag.glsl"
+const vertPath = "shaders/vert.glsl"
+
+func compileShaders() (uint32, uint32) {
+    vertexShaderSource := ghf.LoadFile(vertPath)
+    vertexShader, err := ghf.CreateVertexShader(string(vertexShaderSource))
+    if err != nil {
+        fmt.Printf("Failed to compile vertex shader: %s \n", err)
+    } else {
+        println("Vertex shader compiled successfully")
+    }
+
+    fragmentShaderSource := ghf.LoadFile(fragPath)
+    fragmentShader, err := ghf.CreateFragmentShader(string(fragmentShaderSource))
+    if err != nil {
+        fmt.Printf("Failed to compile fragment shader: %s \n", err)
+    } else {
+        println("Fragment shader compiled successfully")
+    }
+
+    return vertexShader, fragmentShader
+}
+
+func makeProgram(vertexShader, fragmentShader uint32) uint32 {
+    ProgramID, err := ghf.CreateProgram(vertexShader, fragmentShader)
+    if err != nil {
+        fmt.Printf("Failed to link Shaders: %s \n", err)
+    } else {
+        println("Program linked successfully")
+    }
+
+    gl.DeleteShader(vertexShader)
+    gl.DeleteShader(fragmentShader)
+
+    return ProgramID
+}
 
 func main() {
     err := sdl.Init(sdl.INIT_EVERYTHING)
@@ -35,21 +71,31 @@ func main() {
     ProgramID := makeProgram(compileShaders())
 
     vertices := []float32 {
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-        0.0, 0.5, 0.0}
+        0.5, 0.5, 0.0, 1.0, 1.0, 
+        0.5, -0.5, 0.0, 1.0, 0.0, 
+        -0.5, -0.5, 0.0, 0.0, 0.0, 
+        -0.5, 0.5, 0.0, 0.0, 1.0, 
+
+        // 0.5, -0.5, 0.0,
+        // -0.5, 0.5, 0.0, 
+        // 0.5, 0.5, 0.0, 
+    }
+
+    indices := []uint32 {
+        0, 1, 3, // triangle 1
+        1, 2, 3, // triangle 2
+    }
     
-    var VBO uint32
-    gl.GenBuffers(1, &VBO)
-    gl.BindBuffer(gl.ARRAY_BUFFER, VBO)
-    var VAO uint32
-    gl.GenVertexArrays(1, &VAO)
-    gl.BindVertexArray(VAO)
+    // VBO := ghf.GenBindBuffers(gl.ARRAY_BUFFER)
+    ghf.GenBindBuffers(gl.ARRAY_BUFFER)
+    VAO := ghf.GenBindArrays()
+    ghf.BufferDataFloat(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+    ghf.GenBindBuffers(gl.ELEMENT_ARRAY_BUFFER)
+    ghf.BufferDataInt(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
 
-    gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
-
-    gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 3*4, nil)
+    gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 5*4, nil)
     gl.EnableVertexAttribArray(0)
+    gl.VertexAttribPointerWithOffset(2, 2, gl.FLOAT, false, 5*4, 2*4)
     gl.BindVertexArray(0)
 
     for {
@@ -64,43 +110,14 @@ func main() {
 
         gl.UseProgram(ProgramID)
         gl.BindVertexArray(VAO)
-        gl.DrawArrays(gl.TRIANGLES, 0, 3)
+        gl.DrawElementsWithOffset(gl.TRIANGLES, int32(len(indices)), gl.UNSIGNED_INT, 0)
 
         window.GLSwap()
 
+        if ghf.CheckShadersforChanges() {
+            ghf.ResetLoadedShaders()
+            ProgramID = makeProgram(compileShaders())
+        }
     }
 } 
 
-func compileShaders() (uint32, uint32) {
-    vertexShaderSource := ghf.LoadFile("shaders/vert.glsl")
-    vertexShader, err := ghf.CreateVertexShader(string(vertexShaderSource))
-    if err != nil {
-        fmt.Printf("Failed to compile vertex shader: %s \n", err)
-    } else {
-        println("Vertex shader compiled successfully")
-    }
-
-    fragmentShaderSource := ghf.LoadFile("shaders/frag.glsl")
-    fragmentShader, err := ghf.CreateFragmentShader(string(fragmentShaderSource))
-    if err != nil {
-        fmt.Printf("Failed to compile fragment shader: %s \n", err)
-    } else {
-        println("Fragment shader compiled successfully")
-    }
-
-    return vertexShader, fragmentShader
-}
-
-func makeProgram(vertexShader, fragmentShader uint32) uint32 {
-    ProgramID, err := ghf.CreateProgram(vertexShader, fragmentShader)
-    if err != nil {
-        fmt.Printf("Failed to link Shaders: %s \n", err)
-    } else {
-        println("Program linked successfully")
-    }
-
-    gl.DeleteShader(vertexShader)
-    gl.DeleteShader(fragmentShader)
-
-    return ProgramID
-}
