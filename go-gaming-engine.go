7f804b90ee424f5384afb401479bcbaa3748e1cb
KCkingcollin <kckingcollin99@gmail.com>
2024-12-20 02:58:35 -0500

    RESIZABLE FUCKING 3D VIEWPORT!!!

--- go-gaming-engine.go
+++ go-gaming-engine.go
@@ -11,8 +11,8 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-const winWidth = 854
-const winHight = 480
+var winWidth int32 = 854
+var winHeight int32 = 480
 const fragPath = "./shaders/quadtexture.frag.glsl"
 const vertPath = "./shaders/main.vert.glsl"
 
@@ -27,7 +27,7 @@ func main() {
 
     window, err := sdl.CreateWindow("GO Gaming Engine",
     sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-    winWidth, winHight,
+    winWidth, winHeight,
     sdl.WINDOW_OPENGL|sdl.WINDOW_RESIZABLE|sdl.WINDOW_BORDERLESS)
     if err != nil {
         panic(err)
@@ -67,6 +67,11 @@ func main() {
     glf.BufferData(gl.UNIFORM_BUFFER, make([]float32, 3*16), gl.DYNAMIC_DRAW)
     gl.BindBufferBase(gl.UNIFORM_BUFFER, 1, UBO1)
 
+    keyboardState := sdl.GetKeyboardState()
+
+    var x float32
+    var z float32
+
     for {
         for event := sdl.PollEvent(); event != nil;  event = sdl.PollEvent() {
             switch event := event.(type) {
@@ -75,8 +80,8 @@ func main() {
             case *sdl.WindowEvent:
                 switch event.Event {
                 case sdl.WINDOWEVENT_SIZE_CHANGED:
-                    width, height := window.GetSize()
-                    gl.Viewport(0, 0, int32(width), int32(height))
+                    winWidth, winHeight = window.GetSize()
+                    gl.Viewport(0, 0, int32(winWidth), int32(winHeight))
                 case sdl.WINDOWEVENT_MOVED:
                     // Handle window move events, maybe log or adjust behavior
                 case sdl.WINDOWEVENT_FOCUS_GAINED:
@@ -86,14 +91,39 @@ func main() {
             }
         }
 
-        gl.Viewport(0, 0, winWidth, winHight)
         gl.ClearColor(0.1, 0.1, 0.1, 1.0)
         gl.Clear(gl.COLOR_BUFFER_BIT)
 
+        // for event := range keyboardState {
+        //     switch event {
+        //     case sdl.SCANCODE_LEFT | sdl.SCANCODE_A:
+        //         x = x - 0.1
+        //     case sdl.SCANCODE_RIGHT | sdl.SCANCODE_D:
+        //         x = x + 0.1
+        //     case sdl.SCANCODE_DOWN | sdl.SCANCODE_S:
+        //         z = z - 0.1
+        //     case sdl.SCANCODE_UP | sdl.SCANCODE_W:
+        //         z = z + 0.1
+        //     }
+        //
+        // }
+        if keyboardState[sdl.SCANCODE_LEFT] != 0 || keyboardState[sdl.SCANCODE_A] != 0 {
+            x = x + .1
+        }
+        if keyboardState[sdl.SCANCODE_RIGHT] != 0 || keyboardState[sdl.SCANCODE_D] != 0 {
+            x = x - .1
+        }
+        if keyboardState[sdl.SCANCODE_DOWN] != 0 || keyboardState[sdl.SCANCODE_S] != 0 {
+            z = z - .1
+        }
+        if keyboardState[sdl.SCANCODE_UP] != 0 || keyboardState[sdl.SCANCODE_W] != 0 {
+            z = z + .1
+        }
+
         ShaderProg1.Use()
-        projectionMatrix := mgl32.Perspective(mgl32.DegToRad(45.0), float32(winWidth)/float32(winHight), 0.1, 100.0)
+        projectionMatrix := mgl32.Perspective(mgl32.DegToRad(45.0), float32(winWidth)/float32(winHeight), 0.1, 100.0)
         viewMatrix := mgl32.Ident4()
-        viewMatrix = mgl32.Translate3D(0.0, 0.0, -3.0)
+        viewMatrix = mgl32.Translate3D(x, 0.0, z)
         Mat4s[2] = projectionMatrix
         Mat4s[1] = viewMatrix
 
